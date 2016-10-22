276f612b469474bc467f183cc553a32302f0a388
WMcKibbin <WilliamS.McKibbin@gmail.com>
2016-10-22 16:08:55 -0400

    fixed options handling for sub commands.

--- index.js
+++ index.js
@@ -7,22 +7,16 @@ var co = require('co');
 var prompt = require('co-prompt');
 
 program
-  .option('-a, --address <address>', 'Ethereum account address')
-  .option('-t, --test <testnet>', 'Set to run on test net')
   .command('run')
-  .action(function() 
-  {
-  	co(function *()
-		{
-			var testnet = false;
-			if (program.testnet)
-			{
-				console.log('setting testnet to true');
-				testnet = true;	
-			}
-			var pass = yield prompt.password('Enter password for this address: ');
-			nebulis.spawnNode(program.address, pass, testnet);
-		});	
-  });
+    .option('-a, --address <address>', 'Ethereum account address')
+    .option('-t, --testnet', 'Set to run on test net')
+    .action(function(options) 
+    {
+      co(function *()
+      { 
+          var pass = yield prompt.password('Enter password for this address: ');
+          nebulis.spawnNode(options.address, pass, options.testnet);
+       });
+    });	
 
 program.parse(process.argv);
