072390361850533e41d10c93b854b3a26fcee1dd
Max <mdb91@pitt.edu>
2016-10-25 18:45:38 -0400

    some comments added

--- nebulis/nebulis.js
+++ nebulis/nebulis.js
@@ -23,20 +23,91 @@ function Nebulis()
 	//safe pointer to this object, use instead of 'this' keyword when inside callbacks
 	var pointer = this; 
 
+	/**
+	*	Start a geth node to run in the background
+	*	@param address the address to unlock
+	*	@param password the password associated with address
+	*	@param isTest {boolean} if true will run geth on testnet
+	*/
+	this.spawnNode = function(address, password, isTest)
+	{
+		//options to start geth w/
+		var geth_params = [ '--rpc','--unlock', address, '--password', password];
+		if (isTest)
+		{
+			geth_params.push('--testnet');
+			console.log('starting geth on testnet...');
+		}
+		//port geth runner will serve from
+		var gethRunnerPort = '8536';
+		//params to init node process w/
+		var gethRunnerParams = ['node_modules/nebulis/gethRunner.js', gethRunnerPort];
+		var options = {detached: true, stdio: 'ignore'};
+		//geth runner params + geth params
+		var params = gethRunnerParams.concat(geth_params);
+		//spawn gethrunner proc
+		var gethRunner = child.spawn('node', params, options);
+		console.log('Geth node runner started with pid: '+gethRunner.pid+' on port:'+ gethRunnerPort);
+		
+		setTimeout(function()
+		{
+			var isSync = false;
+			var isRpc = false;
+			var syncBar;
+			var lastBlock;
+            var _syncCheckInt;
 
-/* 
-	//declare public functions as this.pubFunc = ...
-	this.publicFuntion = function()
-	{
-		console.log('hello everyone');
-	};
-	
-	//declare private functions as function() or var function = ...
-	function privateFunction()
-	{
-		console.log('Can't See me ;p');
+			//connect to gethRunner proc
+			var connection = net.connect({port: 8536}, onConnect);
+		}, 1000);
+		
+		var onConnect = function()
+		{
+			console.log('Connected to geth');
+			var _syncCheck = function() {		
+				if (syncBar)
+				{
+					syncBar.bar.tick(syncBar.sync.currentBlock - lastBlock);
+  					if (syncBar.bar.complete) 
+					{
+    					console.log('\nSync Complete! Ready.\n');
+						process.exit(0);
+  					}
+				}
+			};
+			       
+			connection.on('data', (data) => {
+  				//console.log(data.toString());
+					
+				//check for sync started
+				if(data.toString().includes('Block synchronisation started'))
+				{
+					console.log('Syncing ethereum node...\n');
+					isSync = true;
+				}
+				//check for geth rpc server
+				if(data.toString().includes('HTTP endpoint opened'))
+				{
+					console.log('RPC HTTP endpoint started.\n');
+					isRpc = true;
+				}
+		
+				if (isSync && isRpc)
+				{
+       				syncBar = startSyncProgress(); // the function to run once all flags are true
+					lastBlock = syncBar.start;
+ 					_syncCheckInt = setInterval(_syncCheck, 200);
+				}
+			});
+		}
+
+		gethRunner.unref();
 	}
-*/
+
+	/**
+	*	@private
+	*	Begin keeping track of block download progress
+	*/
 	function startSyncProgress()
 	{
 		console.log('creating sync bar');
@@ -45,7 +116,13 @@ function Nebulis()
 		var sync;
 		while(attempts <= 10 && !sync)
 		{
-			sync = web3.eth.syncing; 
+			sync = web3.eth.syncing;
+			attempts++; 
+		}
+		if (attempts === 11)
+		{
+			console.log("Error: Unable to sync Ethereum node");
+			process.exit(-1);
 		}
 		var bar = new ProgressBar('  downloading [:bar] :percent :etas', {
     			complete: '=',
@@ -57,80 +134,11 @@ function Nebulis()
 		var startBlock = sync.currentBlock;
 		bar.tick(startBlock);	
 		return {
-	       		'sync': sync, 
-	        	'bar': bar,
+	       	'sync': sync, 
+	        'bar': bar,
 			'start': startBlock
 		};
 
 	};
-
-	this.spawnNode = function(address, password, isTest)
-	{
-		var geth_params = [ '--rpc','--unlock', address, '--password', password];
-		if (isTest)
-		{
-			geth_params.push('--testnet');
-			console.log('starting geth on testnet...');
-		}
-		var gethRunnerPort = '8536';
-		var gethRunnerParams = ['node_modules/nebulis/gethRunner.js', gethRunnerPort];
-		var options = {detached: true, stdio: 'ignore'};
-		var params = gethRunnerParams.concat(geth_params);
-		var gethRunner = child.spawn('node', params, options);
-		console.log('Geth node runner started with pid: '+gethRunner.pid+' on port:'+ gethRunnerPort);
-		
-		setTimeout(function()
-		{
-			var isSync = false;
-			var isRpc = false;
-			var syncBar;
-			var lastBlock;
-                        var _syncCheckInt;
-			var connection = net.connect({port: 8536}, () =>{
-				console.log('Connected to geth');
-				var _syncCheck = function() {		
-					if (syncBar)
-					{
-						syncBar.bar.tick(syncBar.sync.currentBlock - lastBlock);
-  						if (syncBar.bar.complete) 
-						{
-    							console.log('\nSync Complete! Ready.\n');
-							process.exit(0);
-  						}
-					}
-				}
-				var _flagCheckInt = setInterval(function() {
-    					if (isSync === true && isRpc === true) {
-        					clearInterval(_flagCheckInt);
-        					syncBar = startSyncProgress(); // the function to run once all flags are true
-						lastBlock = syncBar.start;
- 						_syncCheckInt = setInterval(_syncCheck, 200);
-    					}
-				}, 100); // interval set at 100 milliseconds	
-			       
-				connection.on('data', (data) => {
-  					//console.log(data.toString());
-					
-					//check for sync started
-					if(data.toString().includes('Block synchronisation started'))
-					{
-						console.log('Syncing ethereum node...\n');
-						isSync = true;
-					}
-					//check for geth rpc server
-					if(data.toString().includes('HTTP endpoint opened'))
-					{
-						console.log('RPC HTTP endpoint started.\n');
-						isRpc = true;
-					}
-
-
-				});
-			});
-		}, 1000);
-		
-		gethRunner.unref();
-
-	}
 }
 
