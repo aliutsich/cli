5a9cf9d992c088f07dd1e5e5cf4115d8ebe9b438
Max <mdb91@pitt.edu>
2016-10-25 20:20:32 -0400

    refactor, progress bar stuff

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ var fs = require('fs');
 var nebulis = require('nebulis');
 var co = require('co');
 var prompt = require('co-prompt');
-
+var ProgressBar = require('progress');
 
 
 /*
@@ -19,10 +19,50 @@ program
     .option('-t, --testnet', 'Set to run on test net')
     .action(function(options) 
     {
-      co(function *()
+      var statusCbk = function(status){console.log(status)};
+	  var syncCbk = function(web3Sync)
+	  {
+		var startBlock = web3Sync.currentBlock;
+		var lastBlock = startBlock;
+		console.log('syncCbk, startBlock = '+startBlock+', lastBlock = '+lastBlock);
+		//progress bar object
+		var bar = new ProgressBar('  downloading [:bar] :percent :etas', {
+    			complete: '=',
+    			incomplete: ' ',
+    			width: 60,
+    			total: web3Sync.highestBlock
+  		});
+		console.log('progress bar initialized');
+		//wrapper around prog bar obj
+		var syncBar =  {
+	       	'sync': web3Sync, 
+	        'bar': bar,
+			'start': startBlock
+		};
+		//called on interval to update prog bar
+		var _syncCheck = function() {		
+			if (syncBar)
+			{
+				syncBar.bar.tick(syncBar.sync.currentBlock - lastBlock);
+  				lastBlock = syncBar.sync.currentBlock;
+				if (syncBar.bar.complete) 
+				{
+    				console.log('\nSync Complete! Ready.\n');
+					process.exit(0);
+  				}
+			}
+		};
+		
+		console.log('highestBlock: '+ web3Sync.highestBlock+' currentBlock: '+web3Sync.currentBlock);
+	
+		//start interval
+ 		var _syncCheckInt = setInterval(_syncCheck, 200);
+	  };
+
+	  co(function *()
       { 
           var pass = yield prompt.password('Enter password for this address: ');
-          nebulis.spawnNode(options.address, pass, options.testnet);
+          nebulis.spawnNode(options.address, pass, options.testnet, statusCbk, syncCbk);
        });
     });	
 
--- nebulis/nebulis.js
+++ nebulis/nebulis.js
@@ -29,116 +29,83 @@ function Nebulis()
 	*	@param password the password associated with address
 	*	@param isTest {boolean} if true will run geth on testnet
 	*/
-	this.spawnNode = function(address, password, isTest)
+	this.spawnNode = function(address, password, isTest, statusCbk, syncCbk)
 	{
 		//options to start geth w/
 		var geth_params = [ '--rpc','--unlock', address, '--password', password];
 		if (isTest)
 		{
 			geth_params.push('--testnet');
-			console.log('starting geth on testnet...');
+			statusCbk('starting geth on testnet...');
 		}
 		//port geth runner will serve from
 		var gethRunnerPort = '8536';
+		
 		//params to init node process w/
 		var gethRunnerParams = ['node_modules/nebulis/gethRunner.js', gethRunnerPort];
 		var options = {detached: true, stdio: 'ignore'};
+		
 		//geth runner params + geth params
 		var params = gethRunnerParams.concat(geth_params);
+		
 		//spawn gethrunner proc
 		var gethRunner = child.spawn('node', params, options);
-		console.log('Geth node runner started with pid: '+gethRunner.pid+' on port:'+ gethRunnerPort);
-		
-		setTimeout(function()
-		{
-			var isSync = false;
-			var isRpc = false;
-			var syncBar;
-			var lastBlock;
-            var _syncCheckInt;
-
-			//connect to gethRunner proc
-			var connection = net.connect({port: 8536}, onConnect);
-		}, 1000);
-		
+		statusCbk('Geth node runner started with pid: '+gethRunner.pid+' on port:'+ gethRunnerPort);
+	
 		var onConnect = function()
 		{
-			console.log('Connected to geth');
-			var _syncCheck = function() {		
-				if (syncBar)
-				{
-					syncBar.bar.tick(syncBar.sync.currentBlock - lastBlock);
-  					if (syncBar.bar.complete) 
-					{
-    					console.log('\nSync Complete! Ready.\n');
-						process.exit(0);
-  					}
-				}
-			};
-			       
+			statusCbk('Connected to geth');
+			var isSync=false, isRpc=false;       
 			connection.on('data', (data) => {
   				//console.log(data.toString());
 					
 				//check for sync started
 				if(data.toString().includes('Block synchronisation started'))
 				{
-					console.log('Syncing ethereum node...\n');
+					statusCbk('Syncing ethereum node...\n');
 					isSync = true;
 				}
 				//check for geth rpc server
 				if(data.toString().includes('HTTP endpoint opened'))
 				{
-					console.log('RPC HTTP endpoint started.\n');
+					statusCbk('RPC HTTP endpoint started.\n');
 					isRpc = true;
 				}
-		
+				//set up sync tracking
 				if (isSync && isRpc)
 				{
-       				syncBar = startSyncProgress(); // the function to run once all flags are true
-					lastBlock = syncBar.start;
- 					_syncCheckInt = setInterval(_syncCheck, 200);
+       				startSyncProgress();
 				}
 			});
-		}
-
-		gethRunner.unref();
-	}
-
-	/**
-	*	@private
-	*	Begin keeping track of block download progress
-	*/
-	function startSyncProgress()
-	{
-		console.log('creating sync bar');
-		
-		var attempts = 0;
-		var sync;
-		while(attempts <= 10 && !sync)
-		{
-			sync = web3.eth.syncing;
-			attempts++; 
-		}
-		if (attempts === 11)
-		{
-			console.log("Error: Unable to sync Ethereum node");
-			process.exit(-1);
-		}
-		var bar = new ProgressBar('  downloading [:bar] :percent :etas', {
-    			complete: '=',
-    			incomplete: ' ',
-    			width: 30,
-    			total: sync.highestBlock
-  		});
-		console.log('highestBlock: '+ sync.highestBlock+' currentBlock'+sync.currentBlock);
-		var startBlock = sync.currentBlock;
-		bar.tick(startBlock);	
-		return {
-	       	'sync': sync, 
-	        'bar': bar,
-			'start': startBlock
 		};
 
+		var connection;
+		
+		setTimeout(function()
+		{
+			//connect to gethRunner proc
+			connection = net.connect({port: 8536}, onConnect);
+		}, 1000);
+
+		gethRunner.unref();
+		
+		/**
+		*	@private
+		*	Begin keeping track of block download progress
+		*/
+		function startSyncProgress()
+		{
+			statusCbk('Starting sync tracking...');
+			connection.end();
+			
+			if (!web3.eth.syncing)
+			{
+				statusCbk('Node already sync\'d, returning');
+				process.exit(0);
+			}
+			statusCbk('Not already sync\'d');	
+			syncCbk(web3.eth.syncing);
+		}
 	};
 }
 
